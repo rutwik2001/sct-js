a9f948e11687f9576eb2782f35dcc177664cb9a7
Pasumarthi Rutwik <62496029+rutwik2001@users.noreply.github.com>
2022-03-24 21:18:30 +0530

    Update gasEstimate.js

--- pages/scrt/gasEstimate.js
+++ pages/scrt/gasEstimate.js
@@ -54,7 +54,7 @@ class SCRT extends Component{
             await contract.sendTokens(this.props.account, this.props.tokensCount)
             this.setState({hidden: false})
             this.setState({successMessage: "Your transaction is completed successfully, Redirecting back to profile in 10 seconds"}); 
-            setTimeout(() => { Router.pushRoute(`/main/rewards_log`) }, 10000);
+            setTimeout(() => { Router.pushRoute(`https://dev.smartcookie.in/main/rewards_log`) }, 10000);
             
 
         //
